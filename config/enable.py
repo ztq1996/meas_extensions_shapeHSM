cc1dfa98c9c524ad08082741c790eebd5b735d69
Paul Price <price@astro.princeton.edu>
2016-03-31 15:42:10 -0400

    config: fix activation
    We were activating the base HsmMoments class, which has been removed.

--- config/enable.py
+++ config/enable.py
@@ -5,5 +5,5 @@
 # We activate the REGAUSS PSF-corrected shape measurement, and the adaptive moments of the source and PSF.
 
 import lsst.meas.extensions.shapeHSM
-config.algorithms.names |= ["ext_shapeHSM_HsmShapeRegauss", "ext_shapeHSM_HsmMoments",
+config.algorithms.names |= ["ext_shapeHSM_HsmShapeRegauss", "ext_shapeHSM_HsmSourceMoments",
                             "ext_shapeHSM_HsmPsfMoments"]
