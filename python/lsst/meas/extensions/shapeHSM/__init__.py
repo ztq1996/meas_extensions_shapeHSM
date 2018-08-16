24b221cd2d0f5c31b1ce5bc81457ec5693eca8d7
Russell Owen <rowen@uw.edu>
2018-08-16 12:26:34 -0700

    Remove obsolete from __future__ import...

--- python/lsst/meas/extensions/shapeHSM/__init__.py
+++ python/lsst/meas/extensions/shapeHSM/__init__.py
@@ -22,8 +22,6 @@
 
 """lsst.meas.extensions.shapeHSM
 """
-from __future__ import absolute_import, division, print_function
-
 from lsst.meas.base import BasePlugin, wrapSimpleAlgorithm
 
 from .hsmMomentsControl import *
