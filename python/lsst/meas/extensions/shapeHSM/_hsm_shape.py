b1462fdc00d0caac64892ccb966be5c03f32bf3b
taranu <dtaranu@astro.princeton.edu>
2024-04-24 12:44:23 -0700

    Reraise GalSim RuntimeErrors as MeasurementErrors

--- python/lsst/meas/extensions/shapeHSM/_hsm_shape.py
+++ python/lsst/meas/extensions/shapeHSM/_hsm_shape.py
@@ -259,20 +259,25 @@ class HsmShapePlugin(measBase.SingleFramePlugin):
             # equivalent of each argument for clarity.
             # TODO: [DM-42047] Change to public API when an optimized version
             # is available.
-            galsim._galsim.EstimateShearView(
-                shape._data,  # shape data buffer (not passed in pure Python)
-                image._image,  # gal_image
-                psf._image,  # PSF_image
-                weight._image,  # weight
-                float(skyvar),  # sky_var
-                self.config.shearType.upper(),  # shear_est
-                recomputeFlux.upper(),  # recompute_flux
-                float(2.5 * psfSigma),  # guess_sig_gal
-                float(psfSigma),  # guess_sig_PSF
-                float(precision),  # precision
-                guessCentroid._p,  # guess_centroid
-                hsmparams._hsmp,  # hsmparams
-            )
+            try:
+                galsim._galsim.EstimateShearView(
+                    shape._data,  # shape buffer (not passed in pure Python)
+                    image._image,  # gal_image
+                    psf._image,  # PSF_image
+                    weight._image,  # weight
+                    float(skyvar),  # sky_var
+                    self.config.shearType.upper(),  # shear_est
+                    recomputeFlux.upper(),  # recompute_flux
+                    float(2.5 * psfSigma),  # guess_sig_gal
+                    float(psfSigma),  # guess_sig_PSF
+                    float(precision),  # precision
+                    guessCentroid._p,  # guess_centroid
+                    hsmparams._hsmp,  # hsmparams
+                )
+            # These are GalSim C++ errors. As of v2.5, GalSim does not raise
+            # custom pybind errors so they will all be RuntimeErrors.
+            except RuntimeError as error:
+                raise measBase.MeasurementError(str(error), self.GALSIM.number)
         except galsim.hsm.GalSimHSMError as error:
             raise measBase.MeasurementError(str(error), self.GALSIM.number)
 
